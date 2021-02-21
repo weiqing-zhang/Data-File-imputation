057cbb81b488fbd0c0482814ed531650e35ad438
weiqing-zhang <56125963+weiqing-zhang@users.noreply.github.com>
2021-02-21 18:00:08 -0500

    Update FileUnavaTime.py

--- FileUnavaTime.py
+++ FileUnavaTime.py
@@ -7,6 +7,7 @@ now = datetime.now()
 print(now)
 #import file as myfile
 fileList = ['ES1.csv','ES2.csv']
+# To calculate the number of time interval
 def calcRow(str1, str2):
     t1 = datetime.strptime(str1,'%m/%d/%Y %H:%M')
     t2 = datetime.strptime(str2,'%m/%d/%Y %H:%M')
@@ -15,17 +16,20 @@ def calcRow(str1, str2):
         t1 = t1+timedelta(minutes = 15)
         row = row +1
     return row
-
+# To impute one data table
 def impute(filename):
     myfile = myfile = pd.read_csv(filename)
+    #drop duplicates
     myfile = myfile.drop_duplicates()
+    #fill empty space
     myfile.fillna('null', inplace=True)
     # myfile = pd.concat(myfile, ignore_index=True)
     # get the heading of columns
     heading = myfile.columns.values
     myfile = myfile.reset_index(drop=True)
+    # mylist host time values in the first column
     mylist = myfile[heading[0]].tolist()
-    # mylist is the list of time(values in the first column
+    #the list that the index of "null"
     nullIndex = []
     for i in range(len(mylist)):
         if mylist[i] == 'null':
@@ -33,6 +37,7 @@ def impute(filename):
     if len(nullIndex)>0:
         myfile.drop(index=nullIndex[0], inplace=True)
 
+    # calculate the number of rows in the first column (discounting "null")
     str1 = myfile[heading[0]][0]
     mytime = myfile[heading[0]].tolist()
     while 'null' in mytime:
@@ -42,19 +47,22 @@ def impute(filename):
 
     mylist = myfile[heading[0]].tolist()
 
-    # the index of time added
+    # imputing the table
     i = 0
     while i < row:
         if i > 0:
             currentTime = datetime.strptime(mylist[i], '%m/%d/%Y %H:%M')
             lastTime = datetime.strptime(mylist[i - 1], '%m/%d/%Y %H:%M')
+            # when time interval is correct
             if currentTime == lastTime + timedelta(minutes=15):
                 pass
+            # when time order is wrong, delete them
             elif currentTime <= lastTime:
                 del mylist[i]
                 myfile.drop(i,inplace=True)
                 myfile = myfile.reset_index(drop=True)
                 i=i-1
+            # fill missing energy consupmtion data with empty values and correct time interval
             else:
                 newTime = lastTime + timedelta(minutes=15)
                 newTime = datetime.strftime(newTime, '%m/%d/%Y %H:%M')
@@ -76,7 +84,7 @@ def impute(filename):
 myfile = impute(fileList[0])
 heading = myfile.columns.values
 
-
+#merge th imputed file into one
 for i in range(len(fileList)):
     if i > 0:
         currentFile= impute(fileList[i])
@@ -87,7 +95,7 @@ for i in range(len(fileList)):
 myfile.fillna('null',inplace=True)
 
 
-
+# for the merged file, calculate the sum of each row
 sum = []
 heading = myfile.columns.values
 time = myfile[heading[0]]
@@ -114,7 +122,7 @@ print(now)
 
 myfile.to_csv('ESCo.csv', index=False)
 
-
+# 'return if there is any time interval erro detected. If so, return the index of error
 mylist = myfile[heading[0]].tolist()
 right = True
 index=[]
